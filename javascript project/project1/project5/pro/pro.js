999011200559054862b2a4440a0d85fc6de81d21
swatidhurvey <ss6196704@gmail.com>
2025-09-02 14:05:28 +0530

    Add files via upload

---
+++ javascript project/project1/project5/pro/pro.js
@@ -0,0 +1,72 @@
+let box1 = document.querySelector("#box1")
+let box2 = document.querySelector("#box2")
+let box3 = document.querySelector("#box3")
+let box4 = document.querySelector("#box4")
+let box5 = document.querySelector("#box5")
+let box6 = document.querySelector("#box6")
+let box7 = document.querySelector("#box7")
+
+box1.addEventListener("click",()=>{
+   box1.style.width = "70%"
+   box2.style.width = "5%"
+   box3.style.width = "5%"
+   box4.style.width = "5%"
+   box5.style.width = "5%"
+   box6.style.width = "5%"
+   box7.style.width = "5%"
+})
+
+box2.addEventListener("click",()=>{
+   box1.style.width = "5%"
+   box2.style.width = "70%"
+   box3.style.width = "5%"
+   box4.style.width = "5%"
+   box5.style.width = "5%"
+   box6.style.width = "5%"
+   box7.style.width = "5%"
+})
+box3.addEventListener("click",()=>{
+   box1.style.width = "5%"
+   box2.style.width = "5%"
+   box3.style.width = "70%"
+   box4.style.width = "5%"
+   box5.style.width = "5%"
+   box6.style.width = "5%"
+   box7.style.width = "5%"
+})
+box4.addEventListener("click",()=>{
+   box1.style.width = "5%"
+   box2.style.width = "5%"
+   box3.style.width = "5%"
+   box4.style.width = "70%"
+   box5.style.width = "5%"
+   box6.style.width = "5%"
+   box7.style.width = "5%"
+})
+box5.addEventListener("click",()=>{
+   box1.style.width = "5%"
+   box2.style.width = "5%"
+   box3.style.width = "5%"
+   box4.style.width = "5%"
+   box5.style.width = "70%"
+   box6.style.width = "5%"
+   box7.style.width = "5%"
+})
+box6.addEventListener("click",()=>{
+   box1.style.width = "5%"
+   box2.style.width = "5%"
+   box3.style.width = "5%"
+   box4.style.width = "5%"
+   box5.style.width = "5%"
+   box6.style.width = "70%"
+   box7.style.width = "5%"
+})
+box7.addEventListener("click",()=>{
+   box1.style.width = "5%"
+   box2.style.width = "5%"
+   box3.style.width = "5%"
+   box4.style.width = "5%"
+   box5.style.width = "5%"
+   box6.style.width = "5%"
+   box7.style.width = "70%"
+})
