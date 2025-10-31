a624e5b66be8ed33993c0c603dab4239e5280fd1
swatidhurvey <ss6196704@gmail.com>
2025-11-01 00:09:15 +0530

    Add files via upload

---
+++ Javascript Question/javascript/Qusetion solve/Q1.js
@@ -0,0 +1,493 @@
+// let num =parseFloat(prompt("Enter a number"));
+// if(num>0){
+//     console.log("positive")
+// }
+// else if(num<0){
+//     console.log("negative");
+// }
+// else{
+//     console.log("Zero");
+// }
+// let age=parseInt(prompt("enter your age"));
+// if(age>=18){
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("you can not vote");
+// }
+
+// 
+
+//  let char=prompt("e((nter a latter.").toLowerCase();
+//  if("aeiou".includes(char)){
+//     console.log("vowel");
+//  }
+//  else if(char>='a' && char<='z'){
+//     console.log("constant")
+//  }
+//  else{
+//     console.log("not valid latter");
+//  }
+
+// let a=parseFloat(prompt("enter a  first number"));
+// let b=parseFloat(prompt("enter a second number"));
+// if(a>b){
+//     console.log("first number is greater");
+// }
+// else if(b>a){
+//     console.log("second number is greater");
+// }
+// else{
+//     console.log("Both number are equal");
+// }
+
+// let a=parseFloat(prompt("enter first number:"));
+// let b=parseFloat(prompt("enter a second number:"));
+// let c=parseFloat(prompt("enter third number"));
+// if(a>=b && a>=c){
+//     console.log("first number is largest");
+// }
+// else if(b>=a && b>=c){
+//     console.log("second number is largest");
+// }
+// else
+// {
+//     console.log(" third number is largest");
+// }
+
+// let num=parseFloat(prompt("enter a number"));
+// if(num % 2===0 && num % 3===0){
+//     console.log(" Division by both 2 and 3");
+// }
+// else if(num %2===0){
+//  console.log("Division by 2");
+// }
+// else if(num%3===0){
+//     console.log("Division by 3");
+// }
+// else{
+//     console.log("not division by 2 or 3");
+// }
+
+
+// let char=prompt("enter a single character:")
+// if((char>='a' && char<='z')||(char>='A' && char<='Z'))
+// {
+//     console.log("Alphabet");
+// }
+// else if(char>='0'&& char<=9){
+//     console.log("Digit");
+// }
+// else{
+//     console.log("special character");
+// }
+
+// let percent=parseFloat(prompt("enter your percentage"));
+// if(percent>=90 && percent<=100){
+//     console.log("Grade A");
+// }
+// else if(percent>=80){
+//     console.log("Grade B");
+// }
+// else if(percent>=70){
+//     console.log("Grade C");
+// }
+// else if(percent>=60){
+//     console.log("Grade D");
+// }
+// else{
+//     console.log("Grade F");
+// }
+
+// 
+
+// //////////
+  //  let name=prompt("enter your name:");
+  //  let age =Number(prompt("enter your age"));
+  //  console.log('my name is ${name} & my age is{age}');
+
+  // 
+  // let a=3;
+  // let b=a++ - ++a + --a;
+  // console.log(a,b); 
+
+  // let num=5;
+  // if(num >0){
+  //   console.log("positive")
+  // }
+
+  // let num=-3;
+  // if(num<0){
+  //   console.log("negative");
+  // }
+  // else if(num===0){
+  //   console.log("Zero");
+  // }
+
+  // let num= Number(prompt("enter a number"));
+  // if(num>0){
+  //   console.log("positive");
+  // }
+  // else if(num<0){
+  //   console.log("negative");
+  // }
+  // else
+  // {
+  //   console.log("zero");
+  // }
+
+  // let num=Number(prompt("enter a number"));
+  // if(num %2===0){
+  //   console.log("Even");
+  // }
+  // else{
+  //   console.log("odd");
+  // }
+
+  // let a=10,b=20;
+  // if(a>b){
+  //   console.log(a+"is greater");
+  // }
+  // else{
+  //   console.log(b+"is greater");
+  // }
+  // let num=10;
+  // if(num%5===0){
+  //   console.log("Divisible by 5");
+  // }
+
+  //  let num=7;
+  //  if(num>1 && num%2!==0&&num!==9){
+  //   console.log("Probably prime");
+  //  }
+
+  // let num=Number(prompt("enter a number"));
+  // switch(num){
+  //   case 1:console.log("one");
+  //   break;
+  //   case 2: console.log("two");
+  //   break;
+  //   case 3: console.log("three");
+  //   break;
+  //   default:console.log("Out of range");
+  // } 
+
+  // let age=25,income=30000,creditscore=720;
+  // if(age>=21 && income>=25000 && creditscore>=700){
+  //   console.log("Loan approved");
+  // }
+  // else {
+  //   console.log("Not eligible");
+  // }
+
+  // let age=17;
+  // if(age>=18){
+  //   console.log("eligible to vote:");
+  // }
+  // else{
+  //   console.log("Not eligible:"); 
+  // }
+
+  // let a=10 ,b=5, operator="*";
+  // switch(operator){
+  //   case"+":console.log(a+b);break;
+  //   case"-":console.log(a-b);break;
+  //   case"*":console.log(a*b);break;
+  //   case"/":console.log(a/b);break; 
+  //   default:console.log("Invalid operator");
+  // }
+
+  // let num=50;
+  // if(num>=1 && num<=100){
+  //   console.log("WIthin range");
+  // }
+  // else{
+  //   console.log("out of range:");
+  // }
+
+  // let marks=Number(prompt("enter your marks"));
+  // if(marks>=90 && marks<=100){
+  //    console.log("Grade A +");
+  // }
+  // else if(marks>=80) {
+  //   console.log("Grade A");
+  // }
+  // else if(marks>=70){
+  //   console.log("Grade B+");
+  // }
+  // else if(marks>=60){
+  //   console.log("Grade B");
+  // }
+  // else(marks>=40)
+  // {
+  //   console.log("Grade C");
+  // }
+
+  // let marks=Number(prompt("enter a number"));
+  // switch(true){
+  //   case (marks>=90 && marks<=100):
+  //     console.log("grade A+");
+  //    break;
+  //    case(marks>=80):console.log("grade A");
+  //    break;
+  //    case (marks>=70):console.log("Grade +B");
+  //    break;
+  //    case (marks>=60):console.log("grade B");
+  //    break;
+  //    case (marks>=50):console.log("grade C");
+  //    break;
+  //    case (marks>=40-10):console.log("grade F");
+  //    default:console.log("not grade"); 
+  // }
+
+  // let a=3,b=3,c=3;
+  // if(a===b && b===c){
+  //   console.log("Equilateral");
+  // }
+  // else if(a===b || b===c || a===c){
+  //   console.log("Isosceles");
+  // }
+  // else{
+  //   console.log("Scalene");
+  // }
+
+  // let username="Admin",password="123";
+  // if(username==="Admin"&& password==="123"){
+  //   console.log("Login Successful");
+  // }
+  // else{
+  //   console.log("Login failed");
+  // }
+
+  // let username="Admin",password="123";
+  // if(username==="Admin"&& password==="123"){
+  //   console.log("Login Successful");
+  // }
+  // else{
+  //   console.log("Login faild")
+  // }
+  //  let code="Male";
+  //  if(code==="m"){
+  //    console.log("Male")
+  //  }
+  //  else if(code==="F"){
+  //   console.log("Female");
+  //  }
+  //  else{
+  //   console.log("Other");
+  //  }
+
+  // let gender=prompt("enter gender");
+  // if( gender === "f"){
+  //   console.log("female");
+  // }
+  // else if( gender === "m"){
+  //   console.log("male");
+  // }
+  // else{
+  //   console.log("other");
+  // }
+
+  //  let a=parseInt(prompt("enter first value:"));
+  //  let b=parseInt(prompt("enter second value:"));
+  //  let marks=a;
+  //  a=b;
+  //  b=marks;
+
+  //  console.log("a=",a);
+  //  console.log("b=",b);
+
+  // let a=parseInt(prompt("Enter first value:"));
+  // let b=parseInt(prompt("Enter second value:"));
+  // a=a+b;
+  // b=a-b;
+  // a=a-b;
+  // console.log("a=",a);
+  // console.log("b=",a);
+
+// let day=Number(prompt("enter a day"));
+// switch(day){
+//   case 1:console.log("sunday");
+//   break;
+//   case 2:console.log("Monday");
+//   break;
+
+//   case 3:console.log("tuesday");
+//   break;
+//   case 4:console.log("wednesday");
+//   break;
+
+//   case 5:console.log("Thusday");
+//   break;
+
+//   case 6:console.log("Friday");
+//   break;
+//   case 7:console.log("saturaday");
+//   break;
+//   default:
+//   console.log("Invalid day");
+// }  
+
+//  let age=Number(prompt("Enter your age:"));
+//  if(age>=0 && age<=12){
+//   console.log("Child");
+//  }
+//  else if(age>=13 && age<=17){
+//   console.log("Teenger");
+//  }
+//  else if(age>=18 && age<=60){
+//   console.log("Adult");
+//  }
+//  else if(age>60){
+//   console.log("Senior Citizen");
+//  }
+//  else{
+//   console.log("Invalid age");
+//  }
+
+// let days=Number(prompt("Enter a days"));
+// switch(days){
+//   case 1:console.log("sunday");break;
+//   case 2:console.log("Monday");break;
+//   case 3:console.log("tuesday");break;
+//   case 4:console.log("wednesday");break;
+//   case 5:console.log("thusday");break;
+//   case 6:console.log("friday");break;
+//   case 7:console.log("saturady");break;
+//   default:console.log("invalid");break;
+// }
+
+
+// let year=Number(prompt("enter a leap year:"));
+// if((year%4===0 && year%100!==0||year%400===0));
+// {
+//   console.log("leap year");
+// }
+// else if {
+//   console.log("not leap year");
+// }
+
+// let year=Number(prompt("Enter a leap year"));
+// if((year%4===0 && year%100!==0 )|| year%400===0)
+// {
+//   console.log("Leap year");  
+// }
+// else  {
+//   console.log("not leap year")
+// }
+
+    // let price=1200;
+    // let quantity=12;
+    // if(price>1000 && quantity>10){
+    //   discount=15;
+    // }
+    // else if(price>1000){
+    //   discount=10;
+    // }
+    // else if(quantity>10){
+    //   discount=5;
+    // }
+    // else{
+    //   discount=0;
+    // }
+    // let total=price*quantity;
+    // let discountAmount=(total*discount)/100;
+    // let finalAmount=total-discountAmount;
+
+    // console.log("total:"+total);
+    // console.log("Discount:"+discount+"%");
+    // console.log("Amount to pay:"+finalAmount);
+
+    // let units=250;
+    // let bill=0;
+    // if(units<=100){
+    //   bill=units*5;
+    // }
+    // else if(units<=200){
+    //   bill=(100*5)+((units-100)*7);
+    // }
+    // else{
+    //   bill=(100*5)+(100*7)+((units-200)*10);
+    // }
+    // console.log("Electricity bill for"+units+"units is:rupye="+bill);
+
+    // let unit=Number(prompt("enter your bill:"));
+    // bill=0;
+    // if(unit>100){
+    //   bill=0;
+    // }
+    // if(unit>100 && unit<=200){
+    //   bill=(100-200)*5;
+    // }
+    // if(unit>300 && unit<=500){
+    //   bill=(300-500)*5;
+    // }
+    // if(unit>500){
+    //   bill=1200;
+    // }
+    // totalbill=(bill);
+    // console.log("your electricity bill="+totalbill);
+    // let i=5;
+    
+    // for(i=0;i<5;i++){
+    //   console.log('i');
+    // }
+
+// let arr=["swati","dhurvey"];
+// for(let i=0;i<arr.length;i++){
+//   console.log([arr],i)
+// }
+
+// let arr=[1,2,3,5,6]
+// let stack=[];
+// for(let i=0;i<arr.length;i++){
+//   stack.push(arr[i]);
+// }
+// console.log("stack=",stack)
+
+// let arr=[1,2,3,4,5]
+// {
+//   arr.push("apple");
+// }
+// console.log(arr)
+
+// let arr=[12,13,15,"swati"]
+// let stack=[];
+//   for(let i=0;i<arr.length;i++){
+//     stack.push(stack[i],"swati")
+//   }
+//   console.log("stack=",stack)
+//   console.log(arr,stack)
+
+// for(i=1;i<21;i++){
+//   if(i%2===1){
+//     console.log(i)
+//   }
+// }
+// let arr=[1,2,3,4,5]
+// for(i=0;i<=5;i++){
+//   arr.push("swati");
+//   console.log(arr)
+// }
+// let ch=prompt("enter a charcter:")
+// ch=ch.toLowerCase();
+// if(ch==='a'||ch==='e'||ch==='i'||ch==='o'||ch==='u'){
+//   console.log(ch+"vovels");
+// }
+// else if(ch>='a'&& ch<='z'){
+//   console.log(ch+"is a constant");
+// }
+// else{
+//   console.log("Please enter a valid alphabet(A-Z)")
+// }
+
+let box=document.querySelector('#box')
+let btn=document.querySelector('button')
+btn.addEventListener('click',()=>{
+  box.style.backgroundColor='red';
+  box.innerHTML='hello'
+  box.style.width=100+'px';
+  box.style.height=100+'px';
+
+})
+ 
